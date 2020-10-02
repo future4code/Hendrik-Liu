ebf1728718ab3337d86dd4333ef4ce6d1b295f97
Hendrik Liam I Liu <hendrikliu.12@gmail.com>
2020-10-02 16:08:00 -0300

    projeto blackjack com desafio incompleto

---
+++ semana3/blackjack/desafio.js
@@ -0,0 +1,61 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+/*
+let player1 = []
+let player2 = []
+
+console.log("Bem vindo ao jogo de Blackjack!")
+//Inicia rodada
+if (confirm("Quer iniciar uma nova rodada?")){
+   //Compra cartas
+   let p1C1 = comprarCarta()
+   let p1C2 = comprarCarta()
+   let p2C1 = comprarCarta()
+   let p2C2 = comprarCarta()
+
+   //Verifica cartas Às
+   if (p1C1.valor != 11 && p1C2.valor != 11){
+      player2.push(p1C1)
+      player2.push(p1C2)
+   } else {
+      p1C1 = comprarCarta()
+      p1C2 = comprarCarta()
+      player1.push(p1C1)
+      player1.push(p1C2)   
+   } 
+   
+   if (p2C1.valor != 11 && p2C2.valor != 11){
+      player2.push(p1C1)
+      player2.push(p1C2)
+   } else {
+      p2C1 = comprarCarta()
+      p2C2 = comprarCarta()
+      player2.push(p1C1)
+      player2.push(p1C2)
+   } 
+}
+*/
+
+   
+
+
+
+/*
+   //1a Rodada
+   while (confirm("Suas cartas são " + player1[0].texto + " " + player1[1].texto + ". A carta revelada do computador é " + player2[0].texto + ". Deseja comprar mais uma carta?")
+      player1.push = comprarCarta()
+      let total
+      for (let cartas in player1){
+         totalP1 = totalP1 + player1.valor[i];
+      }
+*/
