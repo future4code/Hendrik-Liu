ec6fa1ba5bea59e01624fb0b8bd866658732aab8
Hendrik Liu <68404330+hendriklil@users.noreply.github.com>
2021-02-08 16:02:15 -0300

    exercicios de testes

---
+++ semana20/aula58/jest-template/tests/exercicio2.test.ts
@@ -0,0 +1,43 @@
+import { User, performPurchase } from "../src/exercicio2"
+
+
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 100
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).toEqual({
+		...user,
+		balance: 50
+	})
+})
+
+
+test("Testing balance equals to value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 50
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).toEqual({
+		...user,
+		balance: 0
+	})
+})
+
+
+test("Testing balance lower than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 30
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).toEqual(undefined)
+})
